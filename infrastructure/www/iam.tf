77df75b660f7bfa3e25257797db393de99502a33
Julian <juliantellezmendez@gmail.com>
2023-07-27 23:11:16 +0100

    chore(app): hello world

---
+++ infrastructure/www/iam.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_user" "www_user" {
+  name = "www_user"
+
+  tags = {
+    application = "${lookup(local.tags, "application")}"
+    environment = "${lookup(local.tags, "environment")}"
+    gitRepo     = "${lookup(local.tags, "git_repo")}"
+    managedBy   = "${lookup(local.tags, "managed_by")}"
+  }
+}
+
+resource "aws_iam_user_policy" "www_user" {
+  name   = aws_iam_user.www_user.name
+  user   = aws_iam_user.www_user.name
+  policy = data.aws_iam_policy_document.www_user.json
+}
+
+data "aws_iam_policy_document" "www_user" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.www_bucket.arn}",
+      "${aws_s3_bucket.www_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_access_key" "www_user" {
+  user = aws_iam_user.www_user.name
+}
+
+output "www_bucket" {
+  value = aws_s3_bucket.www_bucket.id
+}
+
+output "www_access_key_id" {
+  value = aws_iam_access_key.www_user.id
+}
+
+# terraform output www_secret_access_key
+output "www_secret_access_key" {
+  value     = aws_iam_access_key.www_user.secret
+  sensitive = true
+}
