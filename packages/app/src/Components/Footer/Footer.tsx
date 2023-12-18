9f5fd2de1d5e9e999c60d182c83ef8c780aa1d10
juliantellez <juliantellezmendez@gmail.com>
2023-12-18 00:39:48 -0500

    chore(app): add footer

--- packages/app/src/Components/Footer/Footer.tsx
+++ packages/app/src/Components/Footer/Footer.tsx
@@ -1,8 +1,25 @@
 import React from "react"
+import { Atlantic } from "../../Icons/atlantic"
+
+import * as styles from "./Footer.scss"
 
 const Footer = () => {
     return (
-        <div></div>
+        <div className={styles.footer}>
+            <div className={styles.footerGrid}>
+                <div className={styles.footerGridColumn}>
+                    <a href="/" className={styles.footerLogo}>
+                        <Atlantic className={styles.footerLogoIcon} />
+                    </a>
+                </div>
+                <div className={styles.footerCopyright}>
+                    © {new Date().getFullYear()} Atlantic UK
+                </div>
+
+                <div>
+                </div>
+            </div>
+        </div>
     )
 }
 
