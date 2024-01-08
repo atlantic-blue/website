59ce4d0d6a0adb5865b923fd04f5a0f9cba87ae4
juliantellez <juliantellezmendez@gmail.com>
2024-01-08 18:35:54 -0500

    fix: add french page

--- packages/app/src/Pages/router.tsx
+++ packages/app/src/Pages/router.tsx
@@ -1,9 +1,18 @@
 import React from "react"
-import { createBrowserRouter } from "react-router-dom";
+import { RouteObject, createBrowserRouter } from "react-router-dom";
 
 import { ResourceStringLanguage } from "../ResourceStrings/types";
 
 import { PageHome } from "./Home/Home";
+
+const languages: RouteObject[] = (Object.keys(ResourceStringLanguage) as Array<keyof typeof ResourceStringLanguage>).map((key) => {
+    return (
+        {
+            path: `/${ResourceStringLanguage[key]}`,
+            element: <PageHome language={ResourceStringLanguage[key]} />,
+        }
+    )
+})
 
 const router = createBrowserRouter([
     {
@@ -11,22 +20,7 @@ const router = createBrowserRouter([
         element: <PageHome language={ResourceStringLanguage.ENGLISH} />,
         errorElement: <PageHome language={ResourceStringLanguage.ENGLISH} />,
     },
-    {
-        path: "/en",
-        element: <PageHome language={ResourceStringLanguage.ENGLISH} />,
-    },
-    {
-        path: "/es",
-        element: <PageHome language={ResourceStringLanguage.SPANISH} />,
-    },
-    {
-        path: "/pt",
-        element: <PageHome language={ResourceStringLanguage.PORTUGUESE} />,
-    },
-    {
-        path: "/ru",
-        element: <PageHome language={ResourceStringLanguage.RUSSIAN} />,
-    },
+    ...languages,
 ]);
 
 export default router
