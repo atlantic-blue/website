f4ee54690dd10371a8aec30b0c65394bb9df2493
juliantellez <juliantellezmendez@gmail.com>
2024-01-16 02:05:59 -0500

    chore(styles): typed modules

---
+++ packages/www-static/src/components/Header/Header.module.scss.d.ts
@@ -0,0 +1,5 @@
+export declare const header: string;
+export declare const headerGrid: string;
+export declare const headerGridColumn: string;
+export declare const headerLogo: string;
+export declare const headerLogoIcon: string;
